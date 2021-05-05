f30c5d6db6f2141076b9c9b0446736bbfa36205e
TANUJA SANGAMREDDI <TANUJA.Sangamreddi@ibm.com>
2021-05-05 12:19:30 +0530

    first commit

---
+++ src/components/searchbox/search-Box.component.jsx
@@ -0,0 +1,10 @@
+import './search-Box.styles.css';
+
+export const SearchBox= ({placeholder,handleChange}) => (
+    <input className='search' 
+        type="search" 
+        placeholder={placeholder}
+        onChange={handleChange}
+        />
+
+)
